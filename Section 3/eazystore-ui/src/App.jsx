9e68fd1790a24800a7cff8080414436ed7ada7b3
Ashish <ashishpriyadarshan2605@gmail.com>
2026-02-06 18:55:23 +0530

    Used simple < > </> fragment instead of React.Fragement tag

--- Section 3/eazystore-ui/src/App.jsx
+++ Section 3/eazystore-ui/src/App.jsx
@@ -3,14 +3,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/footer/Footer";
-import React from "react";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Header />
       <Footer />
-    </React.Fragment>
+    </>
   );
 }
 
