051804fb3c0b727280435e1f031153d3a14a103a
Ashish <ashishpriyadarshan2605@gmail.com>
2026-02-09 18:29:40 +0530

    Inline Css Styling both approaches {{}} and {js object variable}

--- Section 4/eazystore-ui/src/components/Home.jsx
+++ Section 4/eazystore-ui/src/components/Home.jsx
@@ -3,15 +3,23 @@ import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 
 export default function Home() {
+  const h1Styling = {
+    textAlign: "center",
+    color: "#5B21B6",
+  };
+
   return (
-    <div className="home-container">
-      <PageHeading title="Explore StickERY">
-        <p className="page-heading-paragraph">
-          Add a touch of creativity to your space with our wide range of fun and
-          unique stickers. Perfect for any occasion!
-        </p>
-      </PageHeading>
-      <ProductListings products={products} />
-    </div>
+    <>
+      <h1 style={h1Styling}>Demo of inline CSS</h1>
+      <div className="home-container">
+        <PageHeading title="Explore StickERY">
+          <p className="page-heading-paragraph">
+            Add a touch of creativity to your space with our wide range of fun
+            and unique stickers. Perfect for any occasion!
+          </p>
+        </PageHeading>
+        <ProductListings products={products} />
+      </div>
+    </>
   );
 }
--- Section 4/eazystore-ui/src/components/footer/Footer.jsx
+++ Section 4/eazystore-ui/src/components/footer/Footer.jsx
@@ -3,23 +3,28 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons/faHeart";
 import "./footer.css";
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div>
-        Built with{" "}
-        <FontAwesomeIcon
-          icon={faHeart}
-          className="footer-icon"
-          aria-hidden="true"
-        ></FontAwesomeIcon>{" "}
-        by{" "}
-        <a
-          href="https://github.com/Ashishpriyadarshan"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ashish Priyadarshan
-        </a>
-      </div>
-    </footer>
+    <>
+      <h1 style={{ textAlign: "center", color: "#808080" }}>
+        Showing inline CSS
+      </h1>
+      <footer className="footer">
+        <div>
+          Built with{" "}
+          <FontAwesomeIcon
+            icon={faHeart}
+            className="footer-icon"
+            aria-hidden="true"
+          ></FontAwesomeIcon>{" "}
+          by{" "}
+          <a
+            href="https://github.com/Ashishpriyadarshan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ashish Priyadarshan
+          </a>
+        </div>
+      </footer>
+    </>
   );
 }
